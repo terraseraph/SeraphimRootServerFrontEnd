574c47f4661bbecb38e5d221bdb01e1704d100ff
TerraSeraph <TerraSeraph@gmail.com>
2019-01-19 22:52:57 +1000

    script edit page, forms made

--- SeraphimFrontEnd/src/app/script-editor/script-editor.component.ts
+++ SeraphimFrontEnd/src/app/script-editor/script-editor.component.ts
@@ -23,6 +23,11 @@ export class ScriptEditorComponent implements OnInit {
   hintLoaded: boolean;
   triggerLoaded: boolean;
 
+  eventToEdit: any;
+  actionToEdit: any;
+  hintToEdit: any;
+  triggerToEdit: any;
+
   constructor(
     public dataService: DataService,
     public server: ServerService,
@@ -32,10 +37,10 @@ export class ScriptEditorComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.scriptLoaded = false;
     this.scriptListSubscribe();
     this.scriptSubscribe();
-    this.loadScriptInstances();
+    this.scriptLoaded = false;
+    this.toggleFormPanelOff();
   }
 
   scriptSubscribe() {
@@ -57,9 +62,124 @@ export class ScriptEditorComponent implements OnInit {
     );
   }
 
-  loadScriptInstances() {}
-
   loadScript(scriptName) {
     this.dataService.scriptEditor_setSelectedScript(scriptName);
+  }
+
+  deleteScript(scriptName) {
+    console.log("Deleting: ", scriptName);
+  }
+
+  scriptEditEvent(eventName) {
+    this.findEvent(eventName).then(evt => {
+      this.eventToEdit = evt;
+      this.toggleFormPanel("event");
+    });
+    console.log("Editing event: ", eventName);
+  }
+  scriptEditAction(actionName) {
+    console.log("Editing Action: ", actionName);
+    this.findAction(actionName).then(act => {
+      this.actionToEdit = act;
+      this.toggleFormPanel("action");
+    });
+  }
+
+  scriptEditHint(hintName) {
+    console.log("Editing Hint: ", hintName);
+    this.findHint(hintName).then(hint => {
+      this.hintToEdit = hint;
+      this.toggleFormPanel("hint");
+    });
+  }
+
+  scriptEditTrigger(triggerName) {
+    console.log("Editing Trigger: ", triggerName);
+    this.findAction(triggerName).then(trg => {
+      this.triggerToEdit = trg;
+      this.toggleFormPanel("trigger");
+    });
+  }
+
+  scriptDeleteEvent(eventName) {
+    console.log("Deleting: ", eventName);
+  }
+  scriptDeleteAction(actionName) {
+    console.log("Deleting: ", actionName);
+  }
+
+  scriptDeleteHint(hintName) {
+    console.log("Deleting: ", hintName);
+  }
+
+  scriptDeleteTrigger(triggerName) {
+    console.log("Deleting: ", triggerName);
+  }
+
+  // ================ helpers =============//
+  toggleFormPanel(panelType) {
+    this.toggleFormPanelOff();
+    switch (panelType) {
+      case "event":
+        this.eventLoaded = true;
+        break;
+      case "action":
+        this.actionLoaded = true;
+        break;
+      case "trigger":
+        this.triggerLoaded = true;
+        break;
+      case "hint":
+        this.hintLoaded = true;
+        break;
+      default:
+        break;
+    }
+  }
+
+  toggleFormPanelOff() {
+    this.eventLoaded = false;
+    this.actionLoaded = false;
+    this.hintLoaded = false;
+    this.triggerLoaded = false;
+  }
+  findEvent(name) {
+    return new Promise((resolve, reject) => {
+      for (const evt of this.scriptInstance.events) {
+        if (evt.name === name) {
+          resolve(evt);
+        }
+      }
+    });
+  }
+
+  findAction(name) {
+    return new Promise((resolve, reject) => {
+      for (const act of this.scriptInstance.actions) {
+        if (act.name === name) {
+          resolve(act);
+        }
+      }
+    });
+  }
+
+  findHint(name) {
+    return new Promise((resolve, reject) => {
+      for (const hint of this.scriptInstance.hints) {
+        if (hint.name === name) {
+          resolve(hint);
+        }
+      }
+    });
+  }
+
+  findTrigger(name) {
+    return new Promise((resolve, reject) => {
+      for (const trg of this.scriptInstance.triggers) {
+        if (trg.name === name) {
+          resolve(trg);
+        }
+      }
+    });
   }
 }
