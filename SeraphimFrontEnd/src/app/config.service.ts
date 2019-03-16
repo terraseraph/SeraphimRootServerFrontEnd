367adc057a5414fefc6496ffd299966cc0898e78
TerraSeraph <TerraSeraph@gmail.com>
2019-03-17 05:30:20 +1000

    edits to getting api

--- SeraphimFrontEnd/src/app/config.service.ts
+++ SeraphimFrontEnd/src/app/config.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class ConfigService {
-  public api = `http://192.168.0.180:4300`;
+  public api = `http://localhost:4300`;
   constructor() {}
 
   public getApiUrl() {
