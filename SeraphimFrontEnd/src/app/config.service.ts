0dbec33732c0e59f791743876bfc4757df5ee3f7
TerraSeraph <TerraSeraph@gmail.com>
2019-03-23 04:39:01 +1000

    config update

--- SeraphimFrontEnd/src/app/config.service.ts
+++ SeraphimFrontEnd/src/app/config.service.ts
@@ -4,7 +4,8 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class ConfigService {
-  public api = `http://localhost:4300`;
+  // public api = `http://localhost:4300`;
+  public api = `${location.protocol}//${location.hostname}:4300`;
   constructor() {}
 
   public getApiUrl() {
